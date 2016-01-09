6a328913f5c12b9ed35afba589f1749422bfa2fd
Mick MacCallum <micksmaccallum@gmail.com>
2016-01-09 17:59:32 -0500

    Fixed indentation

--- lib/elixir_test.ex
+++ lib/elixir_test.ex
@@ -1,20 +1,5 @@
 defmodule ElixirTest do
-	def main(args) do
-		IO.puts("hello, world!")
-	end
+  def main(args) do
+    IO.puts("hello, world!")
+  end
 end
-
-defmodule ElixirTest.Api do  
-	use Application
-
-	def start(_type, _args) do
-		import Supervisor.Spec, warn: false
-
-		children = [
-			worker(ElixirTest.Web, [])
-		]
-
-		opts = [strategy: :one_for_one, name: ElixirTest.Supervisor]
-		Supervisor.start_link(children, opts)
-	end
-end 
