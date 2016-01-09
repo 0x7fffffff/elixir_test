fd9a91eda6a758eea1d821b71a46ac52987beb2b
Mick MacCallum <micksmaccallum@gmail.com>
2016-01-09 17:54:12 -0500

    Added ElixirTest.Api definition

--- lib/elixir_test.ex
+++ lib/elixir_test.ex
@@ -3,3 +3,18 @@ defmodule ElixirTest do
 		IO.puts("hello, world!")
 	end
 end
+
+defmodule ElixirTest.Api do  
+	use Application
+
+	def start(_type, _args) do
+		import Supervisor.Spec, warn: false
+
+		children = [
+			worker(ElixirTest.Web, [])
+		]
+
+		opts = [strategy: :one_for_one, name: ElixirTest.Supervisor]
+		Supervisor.start_link(children, opts)
+	end
+end 
